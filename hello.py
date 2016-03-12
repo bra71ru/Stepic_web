f05851854406c3cb643506dde46c20d8a93d18b1
bra71ru <roman@Mac-Pro-Roman.local>
2016-03-12 21:18:19 +0300

    fix

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-def simple_app(environ, start_response):
+def hello(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type','text/plain')]
     start_response(status, response_headers)
