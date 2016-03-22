052bbfc8c2527b28a9060bdf669ff3d0d2513e36
bra71ru <roman@Mac-Pro-Roman.local>
2016-03-22 20:57:53 +0300

    new version

--- init.sh
+++ init.sh
@@ -7,3 +7,10 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart 
 
+#database conf
+mysql -uroot -e "CREATE DATABASE ASK"
+mysql -uroot -e "CREATE USER 'sa'@'localhost' IDENTIFIED BY 'sa'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON ASK.* TO 'sa'@'localhost'"
+
+
+python /home/box/web/ask/manage.py syncdb
